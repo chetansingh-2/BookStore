2b04c5928467aabca565e562802a0b87d444f651
Chetan Singh <109887534+chetansingh-2@users.noreply.github.com>
2023-09-23 19:18:32 +0530

    commit

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,3 @@
-export const PORT = 5555;
-export const mongi = "mongodb+srv://chetankunwar12345:12345@bookstore.4rtjpdm.mongodb.net/?retryWrites=true&w=majority";
+export const PORT = process.env.PORT || 5555;
+export const mongi =
+  "mongodb+srv://chetankunwar12345:12345@bookstore.4rtjpdm.mongodb.net/?retryWrites=true&w=majority";
--- frontend/src/pages/createBook.jsx
+++ frontend/src/pages/createBook.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:5555/books", data)
+      .post("https://bookstore-hdef.onrender.com/books", data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Created successfully", { variant: "success" });
@@ -74,48 +74,43 @@ const CreateBooks = () => {
     //   </div>
     // </div>
 
-
-
-
-
     <div className="p-4">
-  <BackButton />
-  <h1 className="text-3xl my-4">Create Book</h1>
-  {loading ? <Spinner /> : ""}
-  <div className="flex flex-col border-2 border-sky-400 rounded-xl w-full max-w-[600px] mx-auto p-4">
-    <div className="my-4">
-      <label className="text-xl mr-4 text-gray-500">Title</label>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="border-2 border-gray-500 px-4 py-2 w-full"
-      />
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-full max-w-[600px] mx-auto p-4">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+          Save
+        </button>
+      </div>
     </div>
-    <div className="my-4">
-      <label className="text-xl mr-4 text-gray-500">Author</label>
-      <input
-        type="text"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-        className="border-2 border-gray-500 px-4 py-2 w-full"
-      />
-    </div>
-    <div className="my-4">
-      <label className="text-xl mr-4 text-gray-500">Publish Year</label>
-      <input
-        type="number"
-        value={publishYear}
-        onChange={(e) => setPublishYear(e.target.value)}
-        className="border-2 border-gray-500 px-4 py-2 w-full"
-      />
-    </div>
-    <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
-      Save
-    </button>
-  </div>
-</div>
-
   );
 };
 
