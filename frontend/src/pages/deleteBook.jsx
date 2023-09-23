0f72039757df1b4e08ff8e14ef2f87b206475129
Chetan Singh <109887534+chetansingh-2@users.noreply.github.com>
2023-09-23 19:26:02 +0530

    commit

--- frontend/src/pages/deleteBook.jsx
+++ frontend/src/pages/deleteBook.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
@@ -17,17 +17,17 @@ const DeleteBook = () => {
       .delete(`https://bookstore-hdef.onrender.com/books/${id}`)
       .then(() => {
         setLoading(false);
-        enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
-        navigate('/home');
+        enqueueSnackbar("Book Deleted successfully", { variant: "success" });
+        navigate("/home");
       })
       .catch((error) => {
         setLoading(false);
         // alert('An error happened. Please Chack console');
-        enqueueSnackbar('Error', { variant: 'error' });
+        enqueueSnackbar("Error", { variant: "error" });
         console.log(error);
       });
   };
-  
+
   return (
     // <div className='p-4'>
     //   <BackButton />
@@ -45,27 +45,24 @@ const DeleteBook = () => {
     //   </div>
     // </div>
 
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl max-w-md mx-auto p-4">
+        <h3 className="text-2xl text-center">
+          Are you sure you want to delete this book?
+        </h3>
 
-
-    <div className='p-4'>
-  <BackButton />
-  <h1 className='text-3xl my-4'>Delete Book</h1>
-  {loading ? <Spinner /> : ''}
-  <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl max-w-md mx-auto p-4'>
-    <h3 className='text-2xl text-center'>
-      Are you sure you want to delete this book?
-    </h3>
-
-    <button
-      className='p-4 bg-red-600 text-white m-4 w-full'
-      onClick={handleDeleteBook}
-    >
-      Yes, Delete it
-    </button>
-  </div>
-</div>
-
-  )
-}
+        <button
+          className="p-4 bg-red-600 text-white m-4 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DeleteBook;
